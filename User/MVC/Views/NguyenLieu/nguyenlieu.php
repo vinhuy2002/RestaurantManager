c9bffa7e953e3a576953fa1764885ae5f98ead96
Vĩnh Uy <83992296+vinhuy2002@users.noreply.github.com>
2021-10-26 14:41:38 +0700

    Update
    Small update on NguyenLieu

--- User/MVC/Views/NguyenLieu/nguyenlieu.php
+++ User/MVC/Views/NguyenLieu/nguyenlieu.php
@@ -7,23 +7,21 @@
 	</div>
 
 	<div class="card-body">
-		<!-- <div class="table-responsive">
+		<div class="table-responsive">
 			<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
 				<thead>
 					<tr>
 						<th scope="col">ID</th>
-						<th scope="col">Tên bàn</th>
-						<th scope="col">Trạng thái</th>
-						<th scope="col">Đặt trước</th>
+						<th scope="col">Tên nguyên liệu</th>
+						<th scope="col">Số lượng</th>
 						<th scope="col">Thay đổi</th>
 					</tr>
 				</thead>
 				<tbody>
 					<tr>
 						<th scope="row">1</th>
-						<td>Bàn 1</td>
-						<td>Trống</td>
-						<td>18h - 22h</td>
+						<td>Rau</td>
+						<td>200</td>
 						<td>
 							<a href="" type="button" class="btn btn-success btn-rounded" target="_blank">Xem</a>
 							<a href="" type="button" class="btn btn-warning btn-rounded">Sửa</a>
@@ -33,9 +31,8 @@
 
 					<tr>
 						<th scope="row">2</th>
-						<td>Bàn 2</td>
-						<td>Có khách</td>
-						<td>17h - 22h</td>
+						<td>Bơ</td>
+						<td>100</td>
 						<td>
 							<a href="" type="button" class="btn btn-success btn-rounded" target="_blank">Xem</a>
 							<a href="" type="button" class="btn btn-warning btn-rounded">Sửa</a>
@@ -45,9 +42,62 @@
 
 					<tr>
 						<th scope="row">3</th>
-						<td>Bàn vip</td>
-						<td>Trống</td>
-						<td>19h - 22h</td>
+						<td>Sữa</td>
+						<td>50</td>
+						<td>
+							<a href="" type="button" class="btn btn-success btn-rounded" target="_blank">Xem</a>
+							<a href="" type="button" class="btn btn-warning btn-rounded">Sửa</a>
+							<a href="" onclick="return confirm('Bạn có thật sự muốn xóa ?');" type="button" class="btn btn-danger btn-rounded">Xóa</a>
+						</td>
+					</tr>
+
+					<tr>
+						<th scope="row">4</th>
+						<td>Cá</td>
+						<td>100</td>
+						<td>
+							<a href="" type="button" class="btn btn-success btn-rounded" target="_blank">Xem</a>
+							<a href="" type="button" class="btn btn-warning btn-rounded">Sửa</a>
+							<a href="" onclick="return confirm('Bạn có thật sự muốn xóa ?');" type="button" class="btn btn-danger btn-rounded">Xóa</a>
+						</td>
+					</tr>
+
+					<tr>
+						<th scope="row">5</th>
+						<td>Thịt</td>
+						<td>50</td>
+						<td>
+							<a href="" type="button" class="btn btn-success btn-rounded" target="_blank">Xem</a>
+							<a href="" type="button" class="btn btn-warning btn-rounded">Sửa</a>
+							<a href="" onclick="return confirm('Bạn có thật sự muốn xóa ?');" type="button" class="btn btn-danger btn-rounded">Xóa</a>
+						</td>
+					</tr>
+
+					<tr>
+						<th scope="row">6</th>
+						<td>Phô mai</td>
+						<td>75</td>
+						<td>
+							<a href="" type="button" class="btn btn-success btn-rounded" target="_blank">Xem</a>
+							<a href="" type="button" class="btn btn-warning btn-rounded">Sửa</a>
+							<a href="" onclick="return confirm('Bạn có thật sự muốn xóa ?');" type="button" class="btn btn-danger btn-rounded">Xóa</a>
+						</td>
+					</tr>
+					
+					<tr>
+						<th scope="row">7</th>
+						<td>Ớt</td>
+						<td>250</td>
+						<td>
+							<a href="" type="button" class="btn btn-success btn-rounded" target="_blank">Xem</a>
+							<a href="" type="button" class="btn btn-warning btn-rounded">Sửa</a>
+							<a href="" onclick="return confirm('Bạn có thật sự muốn xóa ?');" type="button" class="btn btn-danger btn-rounded">Xóa</a>
+						</td>
+					</tr>
+					<tr>
+						<th scope="row">8</th>
+						<td>Gia vị</td>
+						<td>150</td>
 						<td>
 							<a href="" type="button" class="btn btn-success btn-rounded" target="_blank">Xem</a>
 							<a href="" type="button" class="btn btn-warning btn-rounded">Sửa</a>
@@ -63,7 +113,7 @@
 					$('#dataTable').DataTable();
 				});
 			</script>
-		</div> -->
+		</div>
 	</div>
 </div>
 
